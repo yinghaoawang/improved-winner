e0db60e0da7a770034b6b8c40df4409850c9c5de
Yinghao Wang <yinghao.a.wang@gmail.com>
2024-03-05 12:07:52 -0800

    add event handler & movement event

---
+++ Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,49 @@
+public struct ToolEffect
+{
+
+};
+
+public struct MovementEventData
+{
+    public bool inputX;
+    public bool inputY;
+    public bool isWalking;
+    public bool isRunning;
+    public bool isIdle;
+    public bool isCarrying;
+    public ToolEffect toolEffect;
+    public bool isUsingToolRight;
+    public bool isUsingToolLeft;
+    public bool isUsingToolUp;
+    public bool isUsingToolDown;
+    public bool isLiftingToolRight;
+    public bool isLiftingToolLeft;
+    public bool isLiftingToolUp;
+    public bool isLiftingToolDown;
+    public bool isPickingRight;
+    public bool isPickingLeft;
+    public bool isPickingToolUp;
+    public bool isPickingToolDown;
+    public bool isSwingingToolRight;
+    public bool isSwingingToolLeft;
+    public bool isSwingingToolUp;
+    public bool isSwingingToolDown;
+    public bool idleRight;
+    public bool idleLeft;
+    public bool idleUp;
+    public bool idleDown;
+};
+
+public delegate void MovementDelegate(MovementEventData eventData);
+
+public static class EventHandler
+{
+    public static MovementDelegate MovementEvent;
+
+    public static void CallMovementEvent(MovementEventData eventData)
+    { 
+        if (MovementEvent != null) {
+            MovementEvent(eventData);
+        }
+    }
+}
